1f416823056193b6e568a1645e0a4a2c05458e9c
BlaBlaName <37503202+Mopo3eX@users.noreply.github.com>
2025-12-08 01:44:45 +0300

    Убраны бесполезные комментарии

--- MemeAlerts/Form1.cs
+++ MemeAlerts/Form1.cs
@@ -19,15 +19,10 @@ namespace MemeAlerts
 
         private async void InitializeWebViewAsync()
         {
-            // Инициализируем движок Edge
             await webView21.EnsureCoreWebView2Async(null);
 
-            // Теперь у нас есть доступ к CoreWebView2:
             webView21.CoreWebView2.WebResourceRequested += CoreWebView2_WebResourceRequested;
-            // Можно фильтровать запросы, чтобы ловить ВСЕ (по умолчанию ловятся не все типы)
             webView21.CoreWebView2.AddWebResourceRequestedFilter("*", CoreWebView2WebResourceContext.All);
-
-            // Загружаем страницу MemeAlerts
             webView21.Source = new Uri("https://memealerts.com/");
         }
 
@@ -54,7 +49,7 @@ namespace MemeAlerts
             }
             catch (Exception ex)
             {
-                MessageBox.Show($"Ошибка: {ex.Message}");
+                MessageBox.Show($"ГЋГёГЁГЎГЄГ : {ex.Message}");
             }
         }
         public string GetStreamerID()
@@ -68,7 +63,6 @@ namespace MemeAlerts
             request.Headers.Add("authorization", Bearer);
             request.Headers.Add("if-none-match", "W/\"1312-tjmKxqHt28SF1yj4VQxHjDvTsWA\"");
             request.Headers.Add("priority", "u=1, i");
-            //request.Headers.Add("referer", "https://memealerts.com/settings");
             request.Headers.Add("sec-ch-ua", "\"Chromium\";v=\"142\", \"Brave\";v=\"142\", \"Not_A Brand\";v=\"99\"");
             request.Headers.Add("sec-ch-ua-mobile", "?0");
             request.Headers.Add("sec-ch-ua-platform", "\"Windows\"");
@@ -76,8 +70,6 @@ namespace MemeAlerts
             request.Headers.Add("sec-fetch-mode", "cors");
             request.Headers.Add("sec-fetch-site", "same-origin");
             request.Headers.Add("sec-gpc", "1");
-            //request.Headers.Add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36");
-            //request.Headers.Add("cookie", "__ddg1_=eChzS6lZ2WTuevS45Fuj; __ddg9_=95.31.21.200; __ddg10_=1762019749; __ddg8_=gxkth15nsqXD0nu4");
 
             HttpResponseMessage response = client.SendAsync(request).Result;
             response.EnsureSuccessStatusCode();
